c1f5af987bfac3ea6c61b989d82f5804ccc552ce
Lev <85023254+levjkeR@users.noreply.github.com>
2021-09-08 23:34:24 +0300

    Косяк на косяке ((

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import datetime
 import json
 import time
+import os
 
 import requests
 
@@ -41,7 +42,7 @@ def get_all_members(group_id):
                 'access_token': access_token,
                 'group_id': group_id,
                 'offset': offset * 1000,
-                'fields': 'sex',  # Чтоб разделять аудиторию по полу
+                'fields': 'sex, photo_max_orig',  # Чтоб разделять аудиторию по полу
                 'v': 5.131
             })
             data = r.json()['response']['items']
@@ -51,7 +52,7 @@ def get_all_members(group_id):
         else:
             for item in data:
                 try:
-                    if item['deactivated']:
+                    if item['deactivated'] == 'deleted' or item['deactivated'] == 'banned':
                         members_list['false']['items'].append(item['id'])
                 except KeyError:
                     try:
@@ -59,6 +60,8 @@ def get_all_members(group_id):
                             members_list['male']['items'].append(item['id'])
                         elif item['sex'] == 1:
                             members_list['female']['items'].append(item['id'])
+                            if item['photo_max_orig'][-5:] == 'ava=1':
+                                download_image(item['id'], item['photo_max_orig'])
                         else:
                             members_list['false']['items'].append(item['id'])
                     except KeyError:
@@ -73,9 +76,8 @@ def get_all_members(group_id):
 # Получение информации о пользователе по id
 def get_users(user_ids, fields):
     offsets_list, data = [], []
-
-    for i in range(0, len(user_ids), 1000):
-        offsets_list.append(user_ids[i: 1000 + i])
+    for i in range(0, len(user_ids), 10):
+        offsets_list.append(user_ids[i: 10 + i])
 
     for offset in offsets_list:
         try:
@@ -84,16 +86,33 @@ def get_users(user_ids, fields):
                 'user_ids': offset,
                 'fields': fields,
                 'v': 5.131
-            })
+            }, timeout=5)
+            print(r.json())
             items = r.json()['response']
         except KeyError:
             print('[ERROR] Next try after 500ms!')
-            time.sleep(0.5)
+            print(len(data))
+            t
             get_member_count(user_ids)
         else:
             for item in items:
                 data.append(item)
     return data
+
+
+def download_image(user_id, photo_url):
+    try:
+        r = requests.get(photo_url, stream=True)
+        image_content = r.content
+    except Exception as ex:
+        print('[ERROR] Next try after 500ms!')
+        time.sleep(0.5)
+    else:
+        if not os.path.exists('D:/images'):
+            os.mkdir('D:/images')
+        with open(f'D:/images/{user_id}.jpg', 'wb') as image:
+            image.write(image_content)
+    pass
 
 
 # Пересечение аудитории
@@ -136,6 +155,24 @@ def write_to_file(filename, data):
 
 
 def main():
+    group_list = ['mirea_official', 'mirealab', 'sumirea']
+    females = []
+    for group in group_list:
+        females.append(set(get_all_members(group)['female']['items']))
+    data = list(auditory_union(females))
+    # with open('females.txt', 'w') as file:
+    #     for id in list(data):
+    #         file.write(str(id) + '\n')
+    # data = []
+    # with open('females.txt', 'r') as file:
+    #     for id in file:
+    #         data.append(id[:-1])
+    # print(len(data))
+    # #get_users('levjke, 23123', 'bdate')
+    # users_data = get_users(", ".join(data), 'photo_max_orig')
+    # print(users_data)
+
+
 
 
 if __name__ == '__main__':
